4aa84f8669d8e7280a4a4a6a4fb3d0e56865eb22
tomy21 <tomy21.agung@gmail.com>
2025-06-18 13:55:27 +0700

    updated

--- src/controller/iotController.ts
+++ src/controller/iotController.ts
@@ -33,8 +33,7 @@ export const closeGate = async (req: Request, res: Response) => {
 export const arduinoPing = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-    const { status } = req.body;
-    //   const open = status === "OPEN" ? 1 : 0;
+
     const data = await prisma.occGate.update({
       where: { id: parseInt(id) },
       data: { arduino: 1, updatedAt: new Date() },
