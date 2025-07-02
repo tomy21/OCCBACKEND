0fc3b19133aecc04d64b157e5bd00a01808fbb70
tomy21 <tomy21.agung@gmail.com>
2025-07-02 16:01:38 +0700

    updated

--- src/routes/call/routes.ts
+++ src/routes/call/routes.ts
@@ -199,6 +199,7 @@ export default function createGateStatusRoute(
             };
 
             res.status(200).json({
+              code: 200,
               message: "Gate status fetched and sent to user",
               data: dataGate,
             });
